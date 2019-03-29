fb7489e579f44196db5b8ed96251adc62ac676cb
Hank Barta <hbarta@gmail.com>
2019-03-28 22:52:46 -0500

    start work

--- hahmon.py
+++ hahmon.py
@@ -1,4 +1,9 @@
-#!/usr/bin/python3
-"""
+#!/usr/bin/env python3
+'''
 Monitor hosts 
-"""
+Read output of mosquitto_sub
+e.g. `mosquitto_sub -v -h oak -t \#`
+Process all messages and update the database according to 
+the incoming messages.
+'''
+
