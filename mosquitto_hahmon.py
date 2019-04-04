836ce034d158dff0000c71ca662cabb37f080f43
Hank Barta <hbarta@gmail.com>
2019-04-04 16:35:20 -0500

    code to use with mosquitto_sub if ever implemented

---
+++ mosquitto_hahmon.py
@@ -0,0 +1,28 @@
+import re
+
+
+'''
+Parse a line as it comes from mosquitto_sub program
+'''
+
+
+def parse_MQTT_msg(line):
+    # typical is
+    # 'home_automation/sodus/master_bedroom/temp_humidity 1553831160, 72.50, 30.98'
+    # We ignore up to and including the first slash ('home_automation/sodus/')
+    # (which may some day be shortened to '/ha')
+    # We grab up to the next slash to ID the host ('sodus')
+    # We gab the rest of the topic ('/master_bedroom/temp_humidity')
+    # We grab the timestamp and convert to an integer. ('1553831160')
+    # Return a tuple consisting of (host, topic, timestamp,)
+    # Return None if the line cannot be parsed.
+    # Note: Whereas ...
+    fields = re.split('\W+', line)  # now split on space, comma
+    if len(fields) < 6:     # 4 portions of topic, timestamp and rest of payload
+        return None
+    host = fields[1]
+    topic = '/' + fields[2] + '/' + fields[3]
+    if len(fields) < 4:
+        return None
+    timestamp = int(fields[4])
+    return (host, topic, timestamp,)
